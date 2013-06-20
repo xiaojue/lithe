8720f9006eb5e2cfa744ec5fbe77a656753d938e
xiaojue <designsor@gmail.com>
2013-06-20 15:23:00 +0800

    增加命名空间和代理目录的功能

--- lithe.js
+++ lithe.js
@@ -1,10 +1,8 @@
-/* @author xiaojue[designsor@gmail.com]
- * @fileoverview lithe module
- */
 (function(global, undef) {
 
 	var isBrowser = !! (typeof window !== undef && global.navigator && global.document);
 	if (isBrowser) {
+		var NAMESPACE = {};
 		var doc = global.document,
 		loc = global.location,
 		noop = function() {},
@@ -18,9 +16,13 @@
 		UA = navigator.userAgent,
 		scripts = doc.getElementsByTagName('script'),
 		currentLoadedScript = scripts[scripts.length - 1],
-		BASEPATH = currentLoadedScript.getAttribute('data-path') || currentLoadedScript.src || currentLoadedScript.getAttribute('src'),
-		CONFIG = currentLoadedScript.getAttribute('data-config'),
-		mainjs = currentLoadedScript.getAttribute('data-main'),
+		attr = function(node, ns) {
+			return node.getAttribute(ns);
+		},
+		BASEPATH = attr(currentLoadedScript, 'data-path') || currentLoadedScript.src || attr(currentLoadedScript, 'src'),
+		CONFIG = attr(currentLoadedScript, 'data-config'),
+		DEBUG = attr(currentLoadedScript, 'data-debug') == 'true',
+		mainjs = attr(currentLoadedScript, 'data-main'),
 		baseElement = header.getElementsByTagName('base')[0],
 		commentRegExp = /(\/\*([\s\S]*?)\*\/|([^:]|^)\/\/(.*)$)/mg,
 		jsRequireRegExp = /[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g,
@@ -28,6 +30,11 @@
 		callbacks = {},
 		fetched = {},
 		circularStack = [],
+		anonymouse = [],
+		currentlyAddingScript,
+		interactiveScript,
+		_cgs = [],
+		stack = {},
 		extend = function(source, options) {
 			if (arguments.length === 1) return source;
 			else {
@@ -37,7 +44,6 @@
 				return source;
 			}
 		};
-
 		var tool = extend({
 			isString: function(v) {
 				return toString.call(v) === '[object String]';
@@ -106,14 +112,16 @@
 				node.onload = node.onerror = node.onreadystatechange = function() {
 					if (/loaded|complete|undefined/.test(node.readyState)) {
 						node.onload = node.onerror = node.onreadystatechange = null;
-						if (node.parentNode) node.parentNode.removeChild(node);
+						if (node.parentNode && ! DEBUG) node.parentNode.removeChild(node);
 						node = undef;
 						if (tool.isFunction(cb)) cb();
 					}
 				};
 				node.async = 'async';
 				node.src = url;
+				currentlyAddingScript = node;
 				tool._insertScript(node);
+				currentlyAddingScript = null;
 			},
 			_fetch: function(url, cb) {
 				if (fetched[url]) {
@@ -146,9 +154,26 @@
 				});
 				return tool.unique(deps);
 			},
+			fixNameSpace: function(id, deps) {
+				var ns = tool.findPathNameSpace(id);
+				return tool.map(deps, function(dep) {
+					var depNamespace = tool.getNameSpace(dep);
+					if (ns != depNamespace && depNamespace == '_main') return ns + ':' + dep;
+					return dep;
+				});
+			},
+			findPathNameSpace: function(path) {
+				for (var ns in NAMESPACE) {
+					if (new RegExp('^' + NAMESPACE[ns].basepath).test(path)) return ns;
+				}
+				return '_main';
+			},
 			getPureDependencies: function(mod) {
 				var id = mod.id;
-				return tool.filter(mod.dependencies, function(dep) {
+				//增加命名空间
+				mod.dependencies = tool.fixNameSpace(id, mod.dependencies);
+				//console.log(id,mod.dependencies);
+				var deps = tool.filter(mod.dependencies, function(dep) {
 					circularStack.push(id);
 					var isCircular = tool.isCircularWaiting(module.cache[dep]);
 					if (isCircular) {
@@ -158,6 +183,7 @@
 					circularStack.pop();
 					return ! isCircular;
 				});
+				return tool.createUrl(deps);
 			},
 			isCircularWaiting: function(mod) {
 				if (!mod || mod.status !== module.status.save) return false;
@@ -249,20 +275,176 @@
 				}
 				return tool.normalize(ret);
 			},
+			isAbsolute: function(id) {
+				return id.indexOf('://') > 0 || id.indexOf('//') === 0;
+			},
+			filename: function(path) {
+				return path.slice(path.lastIndexOf('/') + 1).replace(/\?.*$/, '');
+			},
+			getNameSpace: function(id) {
+				if (tool.isAbsolute(id)) return '_main';
+				else {
+					var ns = id.split(':');
+					if (ns[1]) return ns[0];
+					return '_main';
+				}
+			},
+			replaceDir: function(id, directorys) {
+				//只替换一次,且如果路径包含2个dir，也只替换一次,并且只匹配第一个，之后的不匹配
+				// UI:../ -> UI/test = ../test
+				// UI:../ -> UI/UI/test = ../UI/test
+				// UI:../ -> ../a/UI/test = ../a/UI/test [不会替换]
+				// UI:../ -> a/UI/test = a/UI/test [不会替换]
+				var locks = {};
+				for (var k = 0; k < directorys.length; k++) {
+					var dir = directorys[k];
+					for (var j in dir) {
+						var path = dir[j],
+						reg = new RegExp("^" + j + "\/");
+						if (reg.test(id) && ! locks[id]) {
+							id = id.replace(reg, path);
+							locks[id] = true;
+							break;
+						}
+					}
+					if (locks[id]) break;
+				}
+
+				if (tool.isAbsolute(id)) id = tool.normalize(id);
+				return id;
+			},
+			isDir: function(path) {
+				return path.lastIndexOf('/') === path.length - 1;
+			},
+			getAliasDir: function(alias) {
+				var dirs = [];
+				for (var i in alias) {
+					if (tool.isDir(alias[i])) {
+						var dir = {};
+						dir[i] = alias[i];
+						dirs.push(dir);
+					}
+				}
+				return dirs;
+			},
 			createUrl: function(ids) {
 				return tool.map(ids, function(id) {
-					//alias
-					if (ALIAS && ALIAS.hasOwnProperty(id)) id = ALIAS[id];
-					//isAbsolute
-					if (id.indexOf('://') > 0 || id.indexOf('//') === 0) {
-						if (TIMESTAMP) id = id + '?' + TIMESTAMP;
+					//处理namespace
+					var namespace = tool.getNameSpace(id);
+					if (namespace !== '_main') {
+						id = id.split(':')[1];
+					}
+					//说明此文件为config文件
+					if (NAMESPACE.hasOwnProperty(id)) {
+						namespace = id;
+					}
+					var ns = NAMESPACE[namespace];
+					if (ns) {
+						//alias
+						var directorys = tool.getAliasDir(ns.alias);
+						if (ns.alias) {
+							var aliasPath = ns.alias[id];
+							if (aliasPath && ! tool.isDir(aliasPath)) {
+								id = aliasPath;
+							} else if (directorys.length) {
+								id = tool.replaceDir(id, directorys);
+							}
+						}
+						//isAbsolute
+						if (!tool.isAbsolute(id)) {
+							id = tool.resolve(id, ns.basepath);
+						}
+						if (ns.timestamp) id = id + '?' + ns.timestamp;
 						return id;
 					} else {
-						id = tool.resolve(id, BASEPATH);
-						if (TIMESTAMP) id = id + '?' + TIMESTAMP;
-						return id;
+						throw new Error('the namespace ' + namespace + ' is not found!');
 					}
 				});
+			},
+			buildNameSpace: function(cg, cb) {
+				var _main = NAMESPACE['_main'];
+				_main.alias = cg.alias;
+				_main.timestamp = cg.timestamp;
+				tool.addConfigs(cg, function(configs) {
+					tool.forEach(configs, function(config) {
+						var ns = NAMESPACE[config.name];
+						ns.alias = config.alias;
+						ns.timestamp = config.timestamp;
+					});
+					if (cb) cb();
+				});
+			},
+			_hasClear: function(stack, ns) {
+				delete stack[ns];
+				if (tool.keys(stack).length) return false;
+				return true;
+			},
+			addConfigs: function(cg, cb, len) {
+				var oldlen = len || tool.keys(NAMESPACE).length;
+				if (tool.keys(NAMESPACE).length === 1) {
+					cg.name = '_main';
+					cb([cg]);
+					return;
+				}
+				for (var ns in NAMESPACE) {
+					var config = NAMESPACE[ns]['config'];
+					if (ns === '_main') continue;
+					if (stack[ns]) continue;
+					(function(config, ns) {
+						stack[ns] = true;
+						module.use(ns + ':' + config, function(cg) {
+							cg.name = ns;
+							_cgs.push(cg);
+							tool.addNameSpace(cg);
+							var newlen = tool.keys(NAMESPACE).length;
+							if (newlen > oldlen) tool.addConfigs(cg, cb, newlen);
+							var over = tool._hasClear(stack, ns);
+							if (over) cb(_cgs);
+						});
+					})(config, ns);
+				}
+			},
+			addNameSpace: function(cg) {
+				if (cg.hasOwnProperty('namespace')) {
+					for (var i in cg.namespace) {
+						var ns = cg.namespace[i];
+						NAMESPACE[i] = {
+							basepath: ns.basepath,
+							config: ns.config
+						};
+					}
+				}
+			},
+			getCurrentScript: function() {
+				if (currentlyAddingScript) {
+					return currentlyAddingScript;
+				}
+				// For IE6-9 browsers, the script onload event may not fire right
+				// after the the script is evaluated. Kris Zyp found that it
+				// could query the script nodes and the one that is in "interactive"
+				// mode indicates the current script
+				// ref: http://goo.gl/JHfFW
+				if (interactiveScript && interactiveScript.readyState === "interactive") {
+					return interactiveScript;
+				}
+
+				var scripts = header.getElementsByTagName("script");
+
+				for (var i = scripts.length - 1; i >= 0; i--) {
+					var script = scripts[i];
+					if (script.readyState === "interactive") {
+						interactiveScript = script;
+						return interactiveScript;
+					}
+				}
+				return interactiveScript;
+
+			},
+			pathToid: function(path, name) {
+				var ns = tool.findPathNameSpace(path);
+				name = name || tool.filename(path);
+				var url = ns === '_main' ? name: ns + ':' + name;
+				return tool.createUrl([url])[0];
 			}
 		});
 
@@ -284,24 +466,46 @@
 				'compiling': 3,
 				'compiled': 4
 			},
+			_get: function(url) {
+				return module.cache[url] || (module.cache[url] = new module(url));
+			},
+			_save: function(url, meta) {
+				var mod = this._get(url);
+				if (mod.status < module.status.save) {
+					mod.id = url || meta.id;
+					mod.dependencies = meta.deps;
+					mod.factory = meta.factory;
+					mod.status = module.status.save;
+				}
+			},
 			define: function(id, factory) {
 				if (!tool.isString(id) || ! tool.isFunction(factory)) {
 					throw 'define failed';
 				}
-				id = tool.createUrl([id]);
-				var deps = tool.getDependencies(factory.toString()),
-				mod = module.cache[id] || (module.cache[id] = new module(id));
-				if (mod.status < module.status.save) {
-					mod.id = id;
-					mod.dependencies = tool.createUrl(deps);
-					mod.factory = factory;
-					mod.status = module.status.save;
+				var deps = tool.getDependencies(factory.toString());
+				var meta = {
+					deps: deps,
+					factory: factory
+				};
+				if (tool.isAbsolute(id)) meta.id = id;
+				if (!meta.id && doc.attachEvent) {
+					var script = tool.getCurrentScript();
+					if (script) {
+						meta.id = tool.pathToid(script.src);
+					} else {
+						throw new Error('failed to derive:" ' + factory);
+					}
+				}
+				if (meta.id) {
+					module._save(meta.id, meta);
+				} else {
+					meta.id = id;
+					anonymouse.push(meta);
 				}
 			},
 			use: function(ids, cb) {
 				tool.isString(ids) && (ids = [ids]);
 				var urls = tool.createUrl(ids);
-
 				module._fetch(urls, function() {
 					var args = tool.map(urls, function(url) {
 						return url ? module.cache[url]._compile() : null;
@@ -314,6 +518,9 @@
 				loadUris = tool.filter(urls, function(url) {
 					return url && (!module.cache[url] || module.cache[url].status < STATUS.ready);
 				}),
+				restart = function(mod) { (mod || {}).status < STATUS.ready && (mod.status = STATUS.ready); --queue;
+					(queue === 0) && cb();
+				},
 				len = loadUris.length;
 				if (len === 0) {
 					cb();
@@ -321,11 +528,25 @@
 				}
 				var queue = len;
 				for (var i = 0; i < len; i++) { (function(url) {
-						var mod = module.cache[url] || (module.cache[url] = new module(url));
+						//此处远程模块名和本地cache模块名不匹配，需要处理
+						var mod = module._get(url);
 						mod.status < module.status.save ? tool._fetch(url, success) : success();
 						function success() {
 							//before success the define method all ready changed mod and created new dependencies
-							mod = module.cache[url];
+							//如果define初始化不成功，这里根据顺序，修改为成功的mod
+							var len = anonymouse.length;
+							if (len) {
+								if (len > 1) {
+									tool.forEach(anonymouse, function(meta) {
+										meta.id = tool.pathToid(url, meta.id);
+										module._save(meta.id, meta);
+									});
+								} else {
+									module._save(url, anonymouse[0]);
+								}
+								anonymouse = [];
+							}
+							mod = module._get(url);
 							if (mod.status >= STATUS.save) {
 								var deps = tool.getPureDependencies(mod);
 								if (deps.length) {
@@ -341,9 +562,6 @@
 							}
 						}
 					})(loadUris[i]);
-					function restart(mod) { (mod || {}).status < STATUS.ready && (mod.status = STATUS.ready); --queue;
-						(queue === 0) && cb();
-					}
 				}
 			}
 		});
@@ -356,7 +574,14 @@
 				if (mod.status < STATUS.save) return null;
 				mod.status = STATUS.compiling;
 				function require(id) {
-					var child = module.cache[tool.createUrl([id])];
+					//需要处理一下，根据mod增加namespace
+					for (var i in mod.dependencies) {
+						var dep = mod.dependencies[i],
+						ns = tool.getNameSpace(dep);
+						if (ns != '_main' && id == dep.split(':')[1]) id = ns + ':' + id;
+					}
+					id = tool.createUrl([id]);
+					var child = module.cache[id];
 					if (!child) return null;
 					if (child.status === STATUS.compiled) return child.exports;
 					child.parent = mod;
@@ -376,25 +601,37 @@
 
 		//browser api
 		global.define = module.define;
+
 		global.lithe = extend({
 			use: module.use,
-			start: function(mainjs,callback) {
+			start: function(mainjs, callback) {
 				//use by prev config loaded
+				NAMESPACE['_main'] = {
+					basepath: tool.dirname(BASEPATH),
+					config: CONFIG
+				};
 				if (CONFIG) {
 					module.use(CONFIG, function(cg) {
-						if (cg.hasOwnProperty('alias')) ALIAS = cg.alias;
-						if (cg.hasOwnProperty('base')) BASEPATH = cg.base;
-						if (cg.hasOwnProperty('timestamp')) TIMESTAMP = cg.timestamp;
-						module.use(mainjs,callback);
+						tool.addNameSpace(cg);
+						tool.buildNameSpace(cg, function() {
+							var _main = NAMESPACE['_main'];
+							if (cg.basepath) _main.basepath = cg.basepath;
+							if (DEBUG && tool.isFunction(cg.debugswitch)) mainjs = cg.debugswitch(mainjs) || mainjs;
+							module.use(mainjs, callback);
+						});
 					});
 				} else {
-					module.use(mainjs,callback);
+					module.use(mainjs, callback);
 				}
 			}
 		});
 
-        if(mainjs) global.lithe.start(mainjs);
+		if (DEBUG) {
+			global.lithe.cache = module.cache;
+			global.lithe.NAMESPACE = NAMESPACE;
+		}
 
+		if (mainjs) global.lithe.start(mainjs);
 	} else {
 		//node api 
 		exports.tool = require('./lib/lithe-tool.js');
