e625c3fe32c1a899cf76f121e38d7fd4f3d0d45c
xiaojue <designsor@gmail.com>
2013-09-24 14:13:10 +0800

    增加grunt合并文件功能

--- src/wrap.js
+++ src/wrap.js
@@ -1,16 +1,11 @@
-/**
-* @author {{author}}
-* @fileoverview {{fileoverview}}
-* @version {{version}}
-*/
 (function(global, undef) {
 	var isBrowser = !! (typeof window !== undef && global.navigator && global.document);
 	if (isBrowser) {
-		//include tools.js
-	    //include event.js
-		//include path.js
-		//include fetch.js
-		//include module.js
+		include "tools.js"
+	    include "event.js"
+		include "path.js"
+		include "fetch.js"
+		include "module.js"
 		global.lithe = module;
 		global.define = lithe.define;
 		if (mainjs) global.lithe.use(mainjs);
@@ -19,4 +14,3 @@
 		exports.hfs = require('./lib/lithe-hfs.js');
 	}
 })(this);
-
