33bf1f493adc2cbf0f556be9a4c423e3943c7814
Xiaojue <designsor@gmail.com>
2015-04-01 14:32:14 +0800

    fix timeout

--- test/mocha/spec/require.js
+++ test/mocha/spec/require.js
@@ -1,5 +1,7 @@
 describe('#loadjs()', function() {
 
+  this.timeout(10000);
+
   it('lithe 存在 data-path 为绝对路径,也可以设置根目录地址', function(done) {
     lithe.use('test/mods/remote', function(module) {
       expect(module).to.equal('test/mods/remote');
@@ -8,7 +10,6 @@ describe('#loadjs()', function() {
   });
 
   it('可以使用 loadjs 方法加载远程模块', function(done){
-    this.timeout(10000);
     lithe.load('http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js', function(){
       expect(window.jQuery).to.be.a('function');
       done();
@@ -16,7 +17,6 @@ describe('#loadjs()', function() {
   });
 
   it('可以使用 loadjs 方法加载多个远程模块', function(done) {
-    this.timeout(10000);
     lithe.load(['http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js', 'http://cdn.rawgit.com/jashkenas/underscore/master/underscore-min.js', 'http://cdn.rawgit.com/jashkenas/backbone/master/backbone-min.js'], function() {
       expect(window.Backbone).to.be.a('object');
       done();
